7b9547d65b39102e234ca88d06354e85555010d1
Loveday Shadrack <lovedayshadrack@gmail.com>
2026-01-29 02:20:06 +0000

    FastAPI Connection

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+from pydantic import BaseModel
+import pandas as pd
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_DIR = BASE_DIR / "models" / "1st_version"
+
+model = joblib.load(MODEL_DIR / "iris_flower_v1.0.0.joblib")
+
+app = FastAPI(title= "FastAPI for iris Flower type prediction",
+              version="v1.0.0")
+
+@app.get("/model_check")
+async def check_model_status():
+  return {"Model_Status": "Okay",
+          "Load Model": True}
+
+@app.get("/model_info")
+async def info():
+  return {"Project_name": "Iris Flower type prediction based on petal, sepal (length and weight)",
+          "Features": ["sepal length (cm)",
+                        "sepal width (cm)",
+                        "petal length (cm)",
+                        "petal width (cm)",
+                        "target"],
+           "Version": "v1.0.0"}
+
+class Base(BaseModel):
+  sepal_length: float
+  sepal_width: float
+  petal_length: float
+  petal_width: float
+
+@app.post("/predict")
+async def predict(data: Base):
+  try:
+    input_data = pd.DataFrame([{"sepal length (cm)": data.sepal_length,
+                                "sepal width (cm)": data.sepal_width,
+                                "petal length (cm)": data.petal_length,
+                                "petal width (cm)": data.petal_width}])
+    
+    prediction = model.predict(input_data)[0]
+
+    if prediction==0:
+      iris_flower = "Setosa"
+
+    elif prediction==1:
+      iris_flower = "Versicolor"
+
+    else:
+      iris_flower = "Verginca"
+
+    return {"prediction": iris_flower}
+
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
