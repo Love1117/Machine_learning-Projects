ca73f19ba6eaa57aae895dbf52795bcf81b65dd8
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-22 18:10:22 +0100

    Implement Email Spam Detection API with FastAPI

---
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+
+MODEL_DIR = BASE_DIR / "models" / "1st_version"
+
+tfidf_vectorizer = joblib.load(MODEL_DIR / "tfidf_vectorizer.joblib")
+model = joblib.load(MODEL_DIR / "Email-spam-detection_V1.0.0.joblib")
+
+class TextInput(BaseModel):
+    text: str
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the Spam Detection API! Send a POST request to /predict to classify text."}
+
+@app.post("/predict")
+async def predict_spam(text_input: TextInput):
+    new_vec = tfidf_vectorizer.transform([text_input.text])
+
+    prediction = model.predict(new_vec)
+
+    # Interpret the prediction
+    result = "Spam" if prediction[0] == 1 else "Not-Spam"
+    
+    return {"input_text": text_input.text, "prediction": result}
