b0312f7cefbd4a7e3b1c59f0befe71eb094f63cd
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-27 23:24:07 +0100

    Implement database table creation on startup
    Add startup event to create database tables.

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/main.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from app.database.models import Base
+from app.database.session import engine
+
+
 
 app = FastAPI(
     title="Car Price Prediction API",
     description="Production-ready ML API"
 )
 
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+    
 app.include_router(router)
