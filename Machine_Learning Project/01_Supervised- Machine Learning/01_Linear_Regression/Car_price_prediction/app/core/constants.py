858dd06313f84be86a69aafaf64d709b50ca03c5
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-27 23:14:15 +0100

    Add constants for fuel, owner, and seller types

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/core/constants.py
@@ -0,0 +1,13 @@
+Fuel_Columns = ["fuel_Diesel", "fuel_LPG", "fuel_Petrol"]
+Owner_Columns = [
+    "owner_Fourth & Above Owner",
+    "owner_Second Owner",
+    "owner_Test Drive Car",
+    "owner_Third Owner"
+]
+Seller_type_Columns = [
+    "seller_type_Individual",
+    "seller_type_Trustmark Dealer"
+]
+
+Transmission_Map = {"Automatic": 1, "Manual": 0}
