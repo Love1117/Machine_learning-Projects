8f3bd4052f57bd10de4eb8a54ef5eb29d0f168d3
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-28 12:49:51 +0100

    Add API routes for model check and salary prediction

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/api/routes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.prediction_schema import PredictionRequest
+from app.services.predict_service import predict_salary
+from app.database.session import get_db
+
+router = APIRouter()
+
+@router.get("/model_check")
+def model_check():
+    return {"status": "ok"}
+
+
+@router.post("/predict")
+def predict(data: PredictionRequest, db: Session = Depends(get_db)):
+    return predict_salary(data, db)
