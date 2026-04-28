82f1cda3619daa9e61270e1ac2fc4d550bf262d8
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-28 16:00:39 +0100

    Add endpoint to get car model and name options
    Added a new endpoint to retrieve car model and name options.

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/api/routes.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/api/routes.py
@@ -4,8 +4,19 @@ from sqlalchemy.orm import Session
 from app.schemas.prediction_schema import PredictionRequest
 from app.services.predict_service import predict_price
 from app.database.session import get_db
+from app.services.model_loader import car_model_encoder, car_name_encoder
+
 
 router = APIRouter()
+
+@router.get("/options")
+def get_options():
+    return {
+        "car_model_and_year": list(car_model_encoder.keys()),
+        "car_names": list(car_name_encoder.keys())
+    }
+
+
 
 @router.get("/model_check")
 def model_check():
