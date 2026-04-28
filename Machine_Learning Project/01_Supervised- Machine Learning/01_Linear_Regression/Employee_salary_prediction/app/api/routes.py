94c59d343021adcd228ec638f5e4578e57253d54
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-28 16:16:11 +0100

    Add endpoint to get options for prediction
    Added an endpoint to retrieve available options for Country, Race, and Job titles.

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/api/routes.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/api/routes.py
@@ -4,8 +4,18 @@ from sqlalchemy.orm import Session
 from app.schemas.prediction_schema import PredictionRequest
 from app.services.predict_service import predict_salary
 from app.database.session import get_db
+from app.services.model_loader import Country_freq, Race_freq, Job_title_encoder
+
 
 router = APIRouter()
+
+@router.get("/options")
+def get_options():
+    return {
+        "Country": list(Country_freq.keys()),
+        "Race": list(Race_freq.keys()),
+        "Job_title": list(Job_title_encoder.keys())
+    }
 
 @router.get("/model_check")
 def model_check():
