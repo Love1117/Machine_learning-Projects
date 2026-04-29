c84dadec987d2e3efe8be536cfb480b3fe261b67
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-29 18:27:14 +0100

    Add Prediction model for database

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/database/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, nullable=False)
+    predicted_digit = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
