ba31a930af27610ed5c2e045ba1e7e48302b834e
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-29 12:50:36 +0100

    Update prediction service for loan approval logic

--- Machine_Learning Project/01_Supervised- Machine Learning/02_Logistic_Regression/Loan_Approval_Prediction/app/services/predict_service.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/02_Logistic_Regression/Loan_Approval_Prediction/app/services/predict_service.py
@@ -1,75 +1,76 @@
 import pandas as pd
 from fastapi import HTTPException
 
-from app.services.model_loader import model, scale, car_model_encoder, car_name_encoder
-from app.core.constants import Fuel_Columns, Owner_Columns, Seller_type_Columns, Transmission_Map
+from app.services.model_loader import model, scale, Loan_intent_freq, Home_ownership_freq
+from app.core.constants import GENDER, EDUCATION, PREVIOUS_LOAN
 from app.database.crud import save_prediction
 
 
 def prediction(data, db):
 
-    if data.car_ModelAndYear not in car_model_encoder:
+    if data.Loan_intent not in Loan_intent_freq:
         raise HTTPException(
             status_code=400,
             detail={
-                "error": "Invalid car_ModelAndYear",
+                "error": "Invalid Loan_intent",
                 "hint": "Use /options endpoint to see valid values",
-                "examples": list(car_model_encoder.keys())[:5]
+                "examples": list(Loan_intent_freq.keys())[:5]
             }
         )
 
-    if data.car_name not in car_name_encoder:
+    if data.Home_ownership not in Home_ownership_freq:
         raise HTTPException(
             status_code=400,
             detail={
-                "error": "Invalid car_name",
+                "error": "Invalid Home_ownership",
                 "hint": "Use /options endpoint to see valid values",
-                "examples": list(car_name_encoder.keys())[:5]
+                "examples": list(Home_ownership_freq.keys())[:5]
             }
         )
 
-    fuel_encoded = encode_fuel(data.fuel)
-    owner_encoded = encode_owner(data.owner)
-    seller_encoded = encode_seller_type(data.seller_type)
 
-    car_model_val = car_model_encoder[data.car_ModelAndYear]
-    car_name_val = car_name_encoder[data.car_name]
+    Loan_intent_val = Loan_intent_freq[data.Loan_intent]
+    Home_ownership_val = Home_ownership_freq[data.Home_ownership]
 
-    input_dict = {
-        "car_ModelAndYear": car_model_val,
-        "car_name": car_name_val,
-        "year": data.year,
-        "km_driven": data.km_driven,
-        "transmission": Transmission_Map[data.transmission],
-        "mileage": data.mileage,
-        "engine": data.engine,
-        "max_power": data.max_power,
-        "seats": data.seats,
-        **fuel_encoded,
-        **owner_encoded,
-        **seller_encoded
-    }
+    
+    Gender_map = GENDER[data.Gender]
+    Education_map = EDUCATION[data.Education]
+    Previous_loan_map = PREVIOUS_LOAN[data.Previous_loan_defaults_on_file]
 
-    columns = [
-        "car_ModelAndYear",
-        "car_name",
-        "year",
-        "km_driven",
-        "transmission",
-        "mileage",
-        "engine",
-        "max_power",
-        "seats",
-    ] + Fuel_Columns + Owner_Columns + Seller_type_Columns
+    input_data = pd.DataFrame([{"Age":data.Age,
+                                "Gender": Gender_map,
+                                "Education": Education_map,
+                                "Income": data.Income,
+                                "Employment_experience": data.Employment_experience,
+                                "Home_ownership": Home_ownership_val,
+                                "Loan_amount": data.Loan_amount,
+                                "Loan_intent": Loan_intent_val,
+                                "Loan_interest_rate": data.Loan_interest_rate,
+                                "Loan_percent_income": data.Loan_percent_income,
+                                "Credit_history_length": data.Credit_history_length,
+                                "Credit_score": data.Credit_score,
+                                "Previous_loan_defaults_on_file": Previous_loan_map }])
 
-    df = pd.DataFrame([input_dict]).reindex(columns=columns, fill_value=0)
+    input_data = input_data[["Age",
+                             "Gender",
+                             "Education",
+                             "Income",
+                             "Employment_experience",
+                             "Home_ownership",
+                             "Loan_amount",
+                             "Loan_intent",
+                             "Loan_interest_rate",
+                             "Loan_percent_income",
+                             "Credit_history_length",
+                            "Credit_score",
+                            "Previous_loan_defaults_on_file"]]
+    scaled_input = scale.transform(input_data)
 
-    scaled = scaler.transform(df)
-    prediction = float(round(model.predict(scaled)[0], 2))
+    prediction = model.predict(scaled_input)[0]
 
     db_obj = save_prediction(db, data, prediction)
 
     return {
-        "car_price": prediction,
+        "prediction": "Loan_Approved" if prediction == 1 else "Loan_Decline",
         "db_id": db_obj.id
     }
