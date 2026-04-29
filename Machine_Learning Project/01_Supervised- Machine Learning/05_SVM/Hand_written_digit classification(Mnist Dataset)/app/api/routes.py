a162b799eccd4ca151e26f228efa8ea84ab6b271
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-29 18:13:23 +0100

    Create routes.py

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/api/routes.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.prediction_schema import PredictionRequest
+from app.services.predict_service import prediction
+from app.database.session import get_db
+
+
+
+router = APIRouter()
+
+@router.get("/")
+def home():
+  return {"message": "Digit Image Recognition API is running"}
+
+
+@router.get("/model_check")
+def model_check():
+    return {"status": "ok"}
+
+
+@router.post("/predict")
+def predict(data: PredictionRequest, db: Session = Depends(get_db)):
+    return prediction(data, db)
