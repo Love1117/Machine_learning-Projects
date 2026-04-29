6cfc4d68ade626116987140da47f9041cb958f11
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-29 11:36:04 +0100

    Update routes.py

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/api/routes.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Car_price_prediction/app/api/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.schemas.prediction_schema import PredictionRequest
-from app.services.predict_service import predict_price
+from app.services.predict_service import prediction
 from app.database.session import get_db
 from app.services.model_loader import car_model_encoder, car_name_encoder
 
@@ -25,4 +25,4 @@ def model_check():
 
 @router.post("/predict")
 def predict(data: PredictionRequest, db: Session = Depends(get_db)):
-    return predict_price(data, db)
+    return prediction(data, db)
