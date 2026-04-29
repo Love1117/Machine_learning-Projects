c15f1222d0efecfc686a827ff987ed3c789e95ee
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-29 16:50:05 +0100

    Add Prediction model for Iris flower classification

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/database/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sepal_length = Column(Float)
+    sepal_width = Column(Float)
+    petal_length = Column(Float)
+    petal_width = Column(Float)
+    iris_flower = Column(string)
