dda8c505a656b2b42e24b92f73abea870af7ac5a
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-29 20:24:21 +0100

    Refactor prediction endpoint to handle image uploads

--- Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/api/routes.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/api/routes.py
@@ -1,24 +1,25 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 
-from app.schemas.prediction_schema import PredictionRequest
-from app.services.predict_service import prediction
+from app.services.predict_service import predict_digit
 from app.database.session import get_db
-
-
 
 router = APIRouter()
 
 @router.get("/")
 def home():
-  return {"message": "Digit Image Recognition API is running"}
-
+    return {"message": "Digit Image Recognition API is running"}
 
 @router.get("/model_check")
 def model_check():
     return {"status": "ok"}
 
-
 @router.post("/predict")
-def predict(data: PredictionRequest, db: Session = Depends(get_db)):
-    return prediction(data, db)
+async def predict_digit_image(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="File must be an image")
+
+    return await predict_digit(file, db)
