78d703e4c6742d032cfcc7cbed4081b92ed325d5
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-30 12:52:16 +0100

    Update predict_service.py

--- Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/services/predict_service.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/services/predict_service.py
@@ -11,39 +11,16 @@ from app.services.preprocessing import preprocess_image
 from app.database.crud import save_prediction
 
 
-def prediction(data, db):
+def predict_digit(file, db: Session):
+    contents = await file.read()
+        image = Image.open(io.BytesIO(contents))
 
-    
-    input_dict = {
-        "car_ModelAndYear": car_model_val,
-        "car_name": car_name_val,
-        "year": data.year,
-        "km_driven": data.km_driven,
-        "transmission": Transmission_Map[data.transmission],
-        "mileage": data.mileage,
-        "engine": data.engine,
-        "max_power": data.max_power,
-        "seats": data.seats,
-        **fuel_encoded,
-        **owner_encoded,
-        **seller_encoded
-    }
+        # Preprocess
+        features = preprocess_image(image)
 
-    columns = [
-        "car_ModelAndYear",
-        "car_name",
-        "year",
-        "km_driven",
-        "transmission",
-        "mileage",
-        "engine",
-        "max_power",
-        "seats",
-    ] + Fuel_Columns + Owner_Columns + Seller_type_Columns
+        # Predict
+        prediction = model.predict(features)[0]
 
-    df = pd.DataFrame([input_dict]).reindex(columns=columns, fill_value=0)
-
-    scaled = scaler.transform(df)
     prediction = float(round(model.predict(scaled)[0], 2))
 
     db_obj = save_prediction(db, data, prediction)
