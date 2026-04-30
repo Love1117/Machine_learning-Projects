4e9fa204d14d66d37c28c4760e12107ed779efbd
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-04-30 13:10:31 +0100

    Initialize FastAPI application for digit recognition

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.api.routes import router
+from app.database.models import Base
+from app.database.session import engine
+
+
+
+app = FastAPI(
+        title="Handwritten Digit Image Recognition API",
+        version="1.0.0")
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+    
+app.include_router(router)
