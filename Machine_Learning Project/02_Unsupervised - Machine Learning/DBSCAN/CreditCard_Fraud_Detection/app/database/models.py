5e60131c378da019213438b86c307196037ce0e3
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-01 21:16:14 +0100

    Create models.py

---
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/app/database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    step = Column(Float)
+    amount = Column(Float)
+    oldbalanceOrg = Column(Float)
+    newbalanceOrig = Column(Float)
+    oldbalanceDest = Column(Float)
+    newbalanceDest = Column(Float)
+    type_encoded = Column(string)
+    prediction = Column(Float)
