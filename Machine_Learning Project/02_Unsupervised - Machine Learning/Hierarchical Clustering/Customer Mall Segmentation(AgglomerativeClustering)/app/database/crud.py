f80f061caba9584d4a2b6ee021b169636750ced0
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-01 22:13:39 +0100

    Create crud.py

---
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/Hierarchical Clustering/Customer Mall Segmentation(AgglomerativeClustering)/app/database/crud.py
@@ -0,0 +1,17 @@
+from app.database.models import Prediction
+
+def save_prediction(db, data, prediction):
+
+    db_obj = Prediction(
+        Gender=GENDER[data.Gender],
+        Age=data.Age,
+        Annual_Income_k=data.Annual_Income_k,
+        Spending_Score_1_100=data.Spending_Score_1_100,
+        prediction=prediction
+    )
+
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+
+    return db_obj
