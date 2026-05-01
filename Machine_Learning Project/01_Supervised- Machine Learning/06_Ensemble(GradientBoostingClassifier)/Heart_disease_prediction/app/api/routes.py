39486458da1a3a62ff6044ce35ddc057c9534ad7
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-01 21:00:10 +0100

    Update routes.py

--- Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble(GradientBoostingClassifier)/Heart_disease_prediction/app/api/routes.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble(GradientBoostingClassifier)/Heart_disease_prediction/app/api/routes.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.schemas.prediction_schema import PredictionRequest
 from app.services.predict_service import prediction
 from app.database.session import get_db
-from app.services.model_loader import car_model_encoder, car_name_encoder
+
 
 
 router = APIRouter()
