7ccc1a2d8394e4a4936980d0de10dbc6338bdb75
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-01 22:01:43 +0100

    Create models.py

---
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/Hierarchical Clustering/Customer Mall Segmentation(AgglomerativeClustering)/app/database/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    Gender = Column(string)
+    Age = Column(int)
+    Annual_Income_k = Column(Float)
+    Spending_Score_1_100 = Column(Float)
+    prediction = Column(Float)
