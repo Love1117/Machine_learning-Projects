51be510fd475d3bcc843f4da420aed81b985c856
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 16:32:40 +0100

    Update crud.py

--- Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/database/crud.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/app/database/crud.py
@@ -4,7 +4,7 @@ from app.database.models import Prediction
 def save_prediction(db: Session, filename: str, predicted_digit: int):
     db_obj = Prediction(
         filename=filename,
-        predicted_digit=prediction
+        predicted_digit=predicted_digit
     )
 
     db.add(db_obj)
