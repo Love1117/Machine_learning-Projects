90b5f83c224af8d65b7f64a0235f2673ac356934
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 16:29:27 +0100

    Update predict_service.py

--- Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/services/predict_service.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/services/predict_service.py
@@ -6,6 +6,7 @@ from app.database.crud import save_prediction
 
 
 def prediction(data, db):
+  try:
     input_data = pd.DataFrame([{"sepal length (cm)": data.sepal_length,
                                 "sepal width (cm)": data.sepal_width,
                                 "petal length (cm)": data.petal_length,
@@ -22,9 +23,12 @@ def prediction(data, db):
     else:
       iris_flower = "Verginca"
 
-    db_obj = save_prediction(db, data, prediction)
+    db_obj = save_prediction(db, data, iris_flower)
 
     return {
         "prediction": iris_flower,
         "db_id": db_obj.id
     }
+      
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
