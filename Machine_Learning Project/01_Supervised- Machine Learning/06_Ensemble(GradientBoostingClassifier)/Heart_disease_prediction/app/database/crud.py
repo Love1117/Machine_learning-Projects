1822b89f80cecac407a16461c3bfe1b44d97aa65
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 16:45:07 +0100

    Update crud.py

--- Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble(GradientBoostingClassifier)/Heart_disease_prediction/app/database/crud.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble(GradientBoostingClassifier)/Heart_disease_prediction/app/database/crud.py
@@ -4,20 +4,6 @@ from app.database.models import Prediction
 def save_prediction(db, data, prediction):
 
     db_obj = Prediction(
-        car_ModelAndYear=data.car_ModelAndYear,
-        car_name=data.car_name,
-        year=data.year,
-        km_driven=data.km_driven,
-        transmission=1 if data.transmission == "Automatic" else 0,
-        mileage=data.mileage,
-        engine=data.engine,
-        max_power=data.max_power,
-        seats=data.seats,
-        fuel=data.fuel,
-        owner=data.owner,
-        seller_type=data.seller_type,
-        prediction=prediction
-
       gender=GENDER[data.gender],
       height=data.height,
       weight=data.weight,
