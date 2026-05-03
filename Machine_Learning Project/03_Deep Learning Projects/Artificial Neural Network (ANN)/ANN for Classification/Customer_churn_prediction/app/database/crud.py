6be60cdfeba50f633c4578c0e70d81b80161b654
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 14:36:26 +0100

    Update crud.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Customer_churn_prediction/app/database/crud.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Customer_churn_prediction/app/database/crud.py
@@ -1,19 +1,26 @@
 from app.database.models import Prediction
-
+from app.core.constants import GENDER, Senior_Citizen, PARTNER, DEPENDENT, Phone_Sevice, Multi_Lines, Online_Security, Online_Backup, Device_Protection, Tech_Support, Streaming_TV, Streaming_Movies, Peperless_Billings
 def save_prediction(db, data, prediction):
   db_obj = Prediction(
-        car_ModelAndYear=data.car_ModelAndYear,
-        car_name=data.car_name,
-        year=data.year,
-        km_driven=data.km_driven,
-        transmission=1 if data.transmission == "Automatic" else 0,
-        mileage=data.mileage,
-        engine=data.engine,
-        max_power=data.max_power,
-        seats=data.seats,
-        fuel=data.fuel,
-        owner=data.owner,
-        seller_type=data.seller_type,
+        gender=GENDER[data.gender],
+        SeniorCitizen=Senior_Citizen[data.SeniorCitizen],
+        Partner=PARTNER[data.Partner],
+        Dependents=DEPENDENT[data.Dependents],
+        tenure=data.tenure,
+        PhoneService=Phone_Sevice[data.PhoneService],
+        MultipleLines=Multi_Lines[data.MultipleLines],
+        OnlineSecurity=Online_Security[data.OnlineSecurity],
+        OnlineBackup=Online_Backup[data.OnlineBackup],
+        DeviceProtection=Device_Protection[data.DeviceProtection],
+        TechSupport=Tech_Support[data.TechSupport],
+        StreamingTV=Streaming_TV[data.StreamingTV],
+        StreamingMovies=Streaming_Movies[data.StreamingMovies],
+        PaperlessBilling=Peperless_Billings[data.PaperlessBilling],
+        MonthlyCharges=data.MonthlyCharges,
+        TotalCharges=data.TotalCharges,
+        PaymentMethod_status=data.PaymentMethod_status,
+        Contract_status=data.Contract_status,
+        InternetService_status=InternetService_status,
         prediction=prediction
     )
 
