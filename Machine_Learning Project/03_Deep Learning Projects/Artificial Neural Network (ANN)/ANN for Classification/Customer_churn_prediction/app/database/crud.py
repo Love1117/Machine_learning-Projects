7978583c6348ba1580ebe4cbf69ee1353492a69d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 14:48:19 +0100

    Update crud.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Customer_churn_prediction/app/database/crud.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Customer_churn_prediction/app/database/crud.py
@@ -1,6 +1,6 @@
 from app.database.models import Prediction
 from app.core.constants import GENDER, Senior_Citizen, PARTNER, DEPENDENT, Phone_Sevice, Multi_Lines, Online_Security, Online_Backup, Device_Protection, Tech_Support, Streaming_TV, Streaming_Movies, Peperless_Billings
-def save_prediction(db, data, prediction):
+def save_prediction(db, data, prediction_class, probability):
   db_obj = Prediction(
         gender=GENDER[data.gender],
         SeniorCitizen=Senior_Citizen[data.SeniorCitizen],
@@ -20,8 +20,9 @@ def save_prediction(db, data, prediction):
         TotalCharges=data.TotalCharges,
         PaymentMethod_status=data.PaymentMethod_status,
         Contract_status=data.Contract_status,
-        InternetService_status=InternetService_status,
-        prediction=prediction
+        InternetService_status=data.InternetService_status,
+        prediction=prediction_class,
+        probability=probability
     )
 
   db.add(db_obj)
