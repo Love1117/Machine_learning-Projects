d2f0c0c9dfa8ba2502c1696ef680a83d00335283
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 16:24:35 +0100

    Update crud.py

--- Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/database/crud.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/database/crud.py
@@ -1,13 +1,13 @@
 from app.database.models import Prediction
 
-def save_prediction(db, data, prediction):
+def save_prediction(db, data, iris_flower):
 
     db_obj = Prediction(
         sepal_length=data.sepal_length,
         sepal_width=data.sepal_width,
         petal_length=data.petal_length,
         petal_width=data.petal_width,
-        iris_flower=prediction)
+        prediction=iris_flower)
 
     db.add(db_obj)
     db.commit()
