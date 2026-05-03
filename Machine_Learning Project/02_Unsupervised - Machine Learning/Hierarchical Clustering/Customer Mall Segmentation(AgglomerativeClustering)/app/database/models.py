e31d0304467e7a4d4eb907b3256da46c935ca90b
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-03 17:20:39 +0100

    Fix data type casing in models.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/Hierarchical Clustering/Customer Mall Segmentation(AgglomerativeClustering)/app/database/models.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/Hierarchical Clustering/Customer Mall Segmentation(AgglomerativeClustering)/app/database/models.py
@@ -7,8 +7,8 @@ class Prediction(Base):
     __tablename__ = "predictions"
 
     id = Column(Integer, primary_key=True, index=True)
-    Gender = Column(string)
-    Age = Column(int)
+    Gender = Column(String)
+    Age = Column(Integer)
     Annual_Income_k = Column(Float)
     Spending_Score_1_100 = Column(Float)
-    prediction = Column(string)
+    prediction = Column(String)
