6e3624d8a020ae262e3618a11314d51924d5ca82
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-04 20:56:50 +0100

    Create crud.py

---
+++ Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network (CNN)/CIFAR-10 images classification/app/database/crud.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from app.database.models import Prediction
+
+def save_prediction(db: Session, filename, predicted_class_name, confidence):
+    db_obj = Prediction(
+        filename=filename,  
+        predicted_class=predicted_class_name,
+        confidence=confidence)
+
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+
+    return db_obj
