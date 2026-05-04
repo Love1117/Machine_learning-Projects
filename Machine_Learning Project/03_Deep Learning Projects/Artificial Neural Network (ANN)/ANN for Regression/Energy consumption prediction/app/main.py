5146e04c8fcada1e9df0a9564a1eefacdc354455
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-04 13:06:00 +0100

    Create main.py

---
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Regression/Energy consumption prediction/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from app.api.routes import router
+from app.database.models import Base
+from app.database.session import engine
+
+
+
+app = FastAPI(
+    title="Energy Consumption Prediction API",
+    description="Production-ready ML API"
+)
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+    
+app.include_router(router)
