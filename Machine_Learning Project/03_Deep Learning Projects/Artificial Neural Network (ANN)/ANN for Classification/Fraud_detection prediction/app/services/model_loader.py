4a46248ed955401b4bf72796d9b61de379f494fb
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-05 11:32:20 +0100

    Update model_loader.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Fraud_detection prediction/app/services/model_loader.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Fraud_detection prediction/app/services/model_loader.py
@@ -1,5 +1,7 @@
 import joblib
+import tensorflow as tf
+from tensorflow import keras
 from app.core.config import MODEL_DIR
 
-model = models.load_model(MODEL_DIR / "Fraud_detection_prediction_V1.0.0.keras")
+model = keras.models.load_model(MODEL_DIR / "Fraud_detection_prediction_V1.0.0.keras")
 scale = joblib.load(MODEL_DIR / "scale.joblib")
