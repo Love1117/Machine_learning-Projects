cab0556853f2a7febb7e640383d30ba631965ca6
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-05 11:19:01 +0100

    Update model loading to use Keras API

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Customer_churn_prediction/app/services/model_loader.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Customer_churn_prediction/app/services/model_loader.py
@@ -1,5 +1,6 @@
-import joblib
+import tensorflow as tf
+from tensorflow import keras
 from app.core.config import MODEL_DIR
 
-model = models.load_model(MODEL_DIR / "Customer_churn_prediction_V1.0.0.keras")
+model = keras.models.load_model(MODEL_DIR / "Customer_churn_prediction_V1.0.0.keras")
 scale = joblib.load(MODEL_DIR / "scale.joblib")
