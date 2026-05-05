c75af3f48cf41e8d6e0d47688f29cd64dcf0e878
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-05 11:33:46 +0100

    Update model_loader.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Regression/Energy consumption prediction/app/services/model_loader.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Regression/Energy consumption prediction/app/services/model_loader.py
@@ -1,5 +1,7 @@
 import joblib
+import tensorflow as tf
+from tensorflow import keras
 from app.core.config import MODEL_DIR
 
-model = models.load_model(MODEL_DIR / "Smart_home_EnergyConsumption_Prediction_V1.0.0.keras")
+model = keras.models.load_model(MODEL_DIR / "Smart_home_EnergyConsumption_Prediction_V1.0.0.keras")
 scale = joblib.load(MODEL_DIR / "scale.joblib")
