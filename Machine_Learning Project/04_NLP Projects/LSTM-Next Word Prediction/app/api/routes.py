453f15f93dc0ff3a40de83bc660a289fbfdcd0e5
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-07 19:46:11 +0100

    Create routes.py

---
+++ Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/app/api/routes.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.prediction_schema import PredictionRequest
+from app.services.predict_service import prediction
+from app.database.session import get_db
+
+
+
+router = APIRouter()
+
+
+
+@router.get("/model_check")
+def model_check():
+    return {"status": "ok"}
+
+
+@router.post("/predict")
+def predict(request: PredictionRequest, db: Session = Depends(get_db)):
+    return prediction(request, db)
