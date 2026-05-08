0334b0c658d50b2d81ec65a5583119d6861a2306
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-08 18:45:58 +0100

    Create models.py

---
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Text Summarization using_spaCy+TextRank/app/database/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    limit_phrases = Column(Integer)
+    limit_sentences = Column(Integer)
+    summary = Column(String)
