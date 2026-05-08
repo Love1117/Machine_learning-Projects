fac2611cf35e20e718f3cb34f394e52fab03adc8
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-08 19:19:15 +0100

    Update crud.py

--- Machine_Learning Project/04_NLP Projects/Traditional NLP/Text Summarization using_spaCy+TextRank/app/database/crud.py
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Text Summarization using_spaCy+TextRank/app/database/crud.py
@@ -1,9 +1,14 @@
 from app.database.models import Prediction
 
 def save_prediction(db, input_data, summary):
-
     db_obj = Prediction(
-    text = Column(String)
-    limit_phrases = Column(Integer)
-    limit_sentences = Column(Integer)
-    summary = Column(String)
+    text=input_data.text,
+    limit_phrases=input_data.limit_phrases,
+    limit_sentences=input_data.limit_sentences,
+    summary=summary)
+
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+
+    return db_obj
