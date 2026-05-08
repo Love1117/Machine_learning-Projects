49fd864eb01969b861819a90916e46a572e2597e
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-08 12:36:48 +0100

    Implement prediction service for next word generation

---
+++ Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/app/services/predict_service.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException
+
+from app.services.model_loader import model, tokenizer
+from app.services.preprocessing.py import predict_next_words
+from app.database.crud import save_prediction
+
+  
+def prediction(request, db):
+  try:
+    generated_text = predict_next_words(
+            input_word=request.input_word,
+            len_of_words=request.len_of_words,
+            tokenizer=tokenizer,
+            model=model
+        )
+
+    db_obj = save_prediction(db, request, generated_text)
+
+    return {
+        "generated_text": generated_text,
+        "db_id": db_obj.id
+    }
+  except Exception as e:
+    print(f"Prediction error: {e}")
+    import traceback
+    traceback_str = traceback.format_exc()
+    return {"error": f"An internal error occurred during prediction: {e}", "traceback": traceback_str, "generated_text": ""}
