0f91092d07da189602bd1a2c8a00fda48eb9414a
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-08 23:16:42 +0100

    Create routes.py

---
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning (Bert, T5, LLaMa, etc.. (transformers)/Question Answering/app/api/routes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.schemas.prediction_schema import Question
+from app.services.predict_service import prediction
+from app.database.session import get_db
+
+router = APIRouter()
+
+@router.get("/model_check")
+def model_check():
+    return {"status": "ok"}
+
+
+@router.post("/predict")
+def def predict_answer(question_data: Question, db: Session = Depends(get_db)):
+    return prediction(question_data, db)
