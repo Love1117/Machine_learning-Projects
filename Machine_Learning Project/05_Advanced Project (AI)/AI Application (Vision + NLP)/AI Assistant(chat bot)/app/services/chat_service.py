07f5971fd94e069fa32dff40a120c79b3ec37acb
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-12 20:09:11 +0100

    Update chat_service.py

--- Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/services/chat_service.py
+++ Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/services/chat_service.py
@@ -4,13 +4,28 @@ from app.core.config import HF_TOKEN, client
 from app.services.preprocessing import encode_image
 
 
-def prediction(text: str):
-    try:
-        completion = client.chat.completions.create(
-            model="meta-llama/Llama-3.2-3B-Instruct:novita",
-            messages=[{"role": "user", "content": request.text}]
-        )
-        return {"response": completion.choices[0].message.content}
-        
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Llama model error: {e}")
+def predict_chat(question, image):  
+  try:
+    content = [{"type": "text", "text": question}]
+
+    # Handle image if present
+    if image:
+        image_bytes = await image.read()
+        image_b64 = encode_image(image_bytes)
+
+        content.append({
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/png;base64,{image_b64}"}})
+
+    response = client.chat.completions.create(
+            model="google/gemma-3-27b-it",
+            messages=[
+                {"role": "user", "content": content}],
+            max_tokens=200)
+      
+    return {
+            "response": response.choices[0].message.content
+        }
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=f"Llama model error: {e}")
