3af040fb805b1faad892e8c959569d9d8576f161
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-12 18:30:04 +0100

    Update config.py

--- Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/core/config.py
+++ Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/core/config.py
@@ -1,8 +1,16 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
+
 
 load_dotenv()
 
 HF_TOKEN = os.getenv("HF_TOKEN")
 if not HF_TOKEN:
     raise ValueError("HF_TOKEN not found. Check your .env file.")
+
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=HF_TOKEN,
+)
