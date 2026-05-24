63ab9004dcfff5be3237fe030067754f3f8d579a
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-24 17:34:54 +0100

    Update model_loader.py

--- Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Movies_Recommendation_System/app/services/model_loader.py
+++ Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Movies_Recommendation_System/app/services/model_loader.py
@@ -2,6 +2,6 @@ import joblib
 from app.core.config import MODEL_DIR
 
 
-vector = joblib.load(MODEL_DIR / "Vector.joblib")
-similarity = joblib.load(MODEL_DIR / "similarity.joblib")
+vector = joblib.load(MODEL_DIR / "vector.joblib")
+tfidf = joblib.load(MODEL_DIR / "tfidf.joblib")
 movies = joblib.load(MODEL_DIR / "movies.joblib")
