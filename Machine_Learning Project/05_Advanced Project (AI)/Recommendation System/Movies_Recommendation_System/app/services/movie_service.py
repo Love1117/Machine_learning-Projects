e49b67a4d4b88abf3d6c6bbdd4b9b8ebadfbd854
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-24 17:38:59 +0100

    Update movie_service.py

--- Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Movies_Recommendation_System/app/services/movie_service.py
+++ Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Movies_Recommendation_System/app/services/movie_service.py
@@ -1,23 +1,22 @@
 import pandas as pd
 from fastapi import HTTPException
 
-from app.services.model_loader import movies, similarity
+from app.services.model_loader import movies, tfidf, vector
 
 
 def recommendation(request):
     try:
         title = request.title
 
-        
-        idx = movies[movies["Title"] == request.title].index[0]
-        sim_scores = sorted(list(enumerate(similarity[idx])), reverse=True, key=lambda x: x[1])
+        idx = movies[movies["Title"].str.lower() == title.lower()].index[0]
+        sim_scores = sorted(list(enumerate(cosine_similarity(vector[idx], vector).flatten())), reverse=True,  key= lambda item: item[1])
 
         # Get the top 10 similar movies (excluding the movie itself)
         recommendations = []
         for i in sim_scores[1:11]:
             movie_data = movies.iloc[i[0]]
             recommendations.append({"title": movie_data.Title, "poster": movie_data.Poster})
-
+            
         return {"movie": title, 
                 "recommendations": recommendations}
     except IndexError:
