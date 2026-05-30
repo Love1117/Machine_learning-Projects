6b5cdcf01161fb89e65f6bbb8b522dec7e937cc4
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-05-30 01:51:07 +0100

    Update streamlit_app.py

--- Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Music_Recommendation_System/frontend/streamlit_app.py
+++ Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Music_Recommendation_System/frontend/streamlit_app.py
@@ -1,38 +1,102 @@
-services:
+import streamlit as st
+from streamlit_searchbox import st_searchbox
+import requests
+import pandas as pd
+import joblib
+from app.services.model_loader import songs
 
-  fastapi:
 
-    build: .
+# Load songs
+songs = joblib.load("models/songs.joblib")
 
-    container_name: music_api
+# Remove null values
+song_list = sorted(
+    songs["track_name"].dropna().unique().tolist()
+)
 
-    ports:
-      - "8000:8000"
+# Streamlit page
+st.set_page_config(
+    page_title="Music Recommendation System",
+    layout="wide"
+)
 
-    environment:
-      - SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}
-      - SPOTIFY_CLIENT_SECRET=${SPOTIFY_CLIENT_SECRET}
+st.title("🎵 Music Recommendation System")
 
-    volumes:
-      - .:/app
+# Searchable dropdown
+selected_song = st.selectbox(
 
-  streamlit:
+    "Search Song",
 
-    build: .
+    options=song_list,
 
-    container_name: music_streamlit
+    index=None,
 
-    command: streamlit run frontend/streamlit_app.py --server.port 8501 --server.address 0.0.0.0
+    placeholder="Type or select a song"
 
-    ports:
-      - "8501:8501"
+)
 
-    environment:
-      - SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}
-      - SPOTIFY_CLIENT_SECRET=${SPOTIFY_CLIENT_SECRET}
-      
-    volumes:
-      - .:/app
+API_URL = "http://fastapi:8000/recommend"
 
-    depends_on:
-      - fastapi
+# Recommendation button
+if st.button("Recommend"):
+
+    if not selected_song:
+        st.warning("Please select a song")
+
+    else:
+
+        with st.spinner("Getting recommendations..."):
+
+            response = requests.post(
+
+                API_URL,
+
+                json={
+                    "track_name": selected_song
+                }
+
+            )
+
+        if response.status_code == 200:
+
+            data = response.json()
+
+            recommendations = data["recommendations"]
+
+            st.subheader("Recommended Songs")
+
+            cols = st.columns(5)
+
+            for index, song in enumerate(recommendations):
+
+                with cols[index % 5]:
+
+                    # Poster
+                    st.image(
+                        song["poster"],
+                        use_container_width=True
+                    )
+
+                    # Music title
+                    st.markdown(
+                        f"""
+                        <h4 style='margin-bottom:0px;'>
+                        {song['music_title']}
+                        </h4>
+                        """,
+                        unsafe_allow_html=True
+                    )
+
+                    # Artist name
+                    st.markdown(
+                        f"""
+                        <p style='color:gray; margin-top:0px;'>
+                        {song['artist_name']}
+                        </p>
+                        """,
+                        unsafe_allow_html=True
+                    )
+
+        else:
+
+            st.error("Song not found")
