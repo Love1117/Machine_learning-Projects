fea416949fce6ef0302a0778fa37234ab609b6dc
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-08 02:50:06 +0100

    Update product_service.py

--- Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Product_Recommendation_System/app/services/product_service.py
+++ Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Product_Recommendation_System/app/services/product_service.py
@@ -13,20 +13,20 @@ def strikethrough_text(text):
 
 def recommend_products(request: ProductRequest):
   try:
-    requested_brand = request.brand
+    requested_category = request.category
 
-    idx = phone_data[phone_data["brand"].str.lower() == requested_brand.lower()].index
+    idx = products[products["category"].str.lower() == requested_category.lower()].index
 
     if idx.empty:
       raise HTTPException(status_code=404,
-      detail=f"brands '{requested_brand}' not found")
+      detail=f"Category '{requested_category}' not found")
             
     # Use the first matching phone's vector as the reference for similarity
     # Reshape to (1, -1) because cosine_similarity expects 2D arrays
-    brand_reference_vector = vector[idx[0]].reshape(1, -1)
+    category_reference_vector = vector[idx[0]].reshape(1, -1)
 
     # Calculate cosine similarity between the reference vector and all phone vectors
-    sim_scores_raw = cosine_similarity(brand_reference_vector, vector).flatten()
+    sim_scores_raw = cosine_similarity(category_reference_vector, vector).flatten()
 
     # Pair each similarity score with its original index and sort in descending order
     sim_scores = sorted(list(enumerate(sim_scores_raw)),
@@ -36,19 +36,18 @@ def recommend_products(request: ProductRequest):
 
     # Get top 10 recommendations, skipping the first one (which is the input phone itself)
     for i in sim_scores[1:11]:
-      recommended_phone = phone_data.iloc[i[0]]
-      market_price_formatted = strikethrough_text(recommended_phone['market_price'])
+      recommended_products = products.iloc[i[0]]
+      market_price_formatted = strikethrough_text(recommended_products['market_price'])
 
       recommendations.append({
-                "image_url": recommended_phone['image_url'],
-                "product_name": recommended_phone['name'],
-                "rating": f"{recommended_phone['rating']} out of 5 stars ({recommended_phone['ratings_reviews']}) Total comments",
-                "sale_price": f"{recommended_phone['discount']} ${recommended_phone['Sale_Price']}",
+                "image_url": recommended_products['image_url'],
+                "product_name": recommended_products['product'],
+                "sale_price": f"${recommended_products['sale_price']}",
                 "market_price": f"${market_price_formatted}"
             })
     
     return {
-            "requested_phone": requested_brand,
+            "requested_category": requested_category,
             "recommendations": recommendations}
     
   except Exception as e:
