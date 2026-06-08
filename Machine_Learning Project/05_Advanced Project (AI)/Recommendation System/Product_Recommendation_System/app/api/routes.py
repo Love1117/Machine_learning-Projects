045a77754e457855b070a1ed7500c318baf388d9
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-08 02:34:31 +0100

    Create routes.py

---
+++ Machine_Learning Project/05_Advanced Project (AI)/Recommendation System/Product_Recommendation_System/app/api/routes.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from app.schemas.mobile_schema import ProductRequest
+from app.services.mobile_service import recommend_products
+from app.services.model_loader import products 
+
+
+
+router = APIRouter()
+
+
+
+@router.get("/model_check")
+def model_check():
+    return {"status": "ok"}
+
+
+@router.get("/mobile")
+def get_products():
+    product_category = products["category"].tolist()
+    return {
+        "category": product_category
+    }
+
+@router.post("/recommend")
+def get_product_recommendations(request: ProductRequest):
+    return recommend_products(request.category)
