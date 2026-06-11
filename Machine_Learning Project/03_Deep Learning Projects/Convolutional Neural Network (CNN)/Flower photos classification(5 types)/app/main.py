45a9bee7f2b8e04edb045d74cf6bb6a5ce29d735
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-11 03:14:51 +0100

    Create main.py

---
+++ Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network (CNN)/Flower photos classification(5 types)/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.api.routes import router
+from app.database.models import Base
+from app.database.session import engine
+
+
+
+app = FastAPI(
+        title="Flower Photo classification API",
+        version="1.0.0")
+
+@app.get("/")
+def read_root():
+    return {"message": "Flower Photo FastAPI is Running!"}
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+    
+app.include_router(router)
