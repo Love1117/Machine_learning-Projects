0ff202c6597eae3501097c0ae482a53e0f4e1813
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-13 16:41:17 +0100

    Update routes.py

--- Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/classifier_model/app/api/routes.py
+++ Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/classifier_model/app/api/routes.py
@@ -1,1 +1,21 @@
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
+from sqlalchemy.orm import Session
 
+from app.services.predict_service import image_prediction
+from app.database.session import get_db
+
+router = APIRouter()
+
+@router.get("/model_check")
+def model_check():
+    return {"status": "ok"}
+
+@router.post("/predict")
+async def predict_image(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="File must be an image")
+
+    return await image_prediction(file, db)
