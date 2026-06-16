b9f711b9123010e13c66262671927cc492de02ee
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-16 13:10:49 +0100

    Rename prediction parameter to employee_salary

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/database/crud.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/database/crud.py
@@ -1,6 +1,6 @@
 from app.database.models import Prediction
 from app.core.constants import Gend, Senio
-def save_prediction(db, data, prediction):
+def save_prediction(db, data, employee_salary):
 
     db_obj = Prediction(
         Age = data.Age,
@@ -11,7 +11,7 @@ def save_prediction(db, data, prediction):
         Race = data.Race,
         Senior = Senio[data.Senior],
         Job_title = data.Job_title,
-        employee_salary = prediction)
+        employee_salary = employee_salary)
 
     db.add(db_obj)
     db.commit()
