32a1ae9e403e8e93c34ec9a1ba91bd46a72a0d95
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-16 12:39:27 +0100

    Update crud.py

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/database/crud.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/database/crud.py
@@ -1,15 +1,15 @@
 from app.database.models import Prediction
-
+from app.core.constants import Gend, Senio
 def save_prediction(db, data, prediction):
 
     db_obj = Prediction(
         Age = data.Age,
-        Gender = 1 if data.Gender == "Male" else 0,
+        Gender = Gend[data.Gender],
         Education_Level = data.Education_Level,
         Years_of_Experience = data.Years_of_Experience,
         Country = data.Country,
         Race = data.Race,
-        Senior = 1 if data.Senior == "Yes" else 0,,
+        Senior = Senio[data.Senior],
         Job_title = data.Job_title,
         employee_salary = prediction)
 
