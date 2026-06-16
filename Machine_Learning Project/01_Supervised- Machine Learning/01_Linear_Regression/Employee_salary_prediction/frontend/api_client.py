f3cde9d163158a76f325d0d0c00529c4981a5e04
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-16 13:53:05 +0100

    Create api_client.py

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/frontend/api_client.py
@@ -0,0 +1,34 @@
+import requests
+
+FASTAPI_URL = "http://fastapi:8000"
+
+def predict_salary(data):
+    response = requests.post(
+        f"{FASTAPI_URL}/predict",
+        json=data
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def get_country():
+    response = requests.get(
+        f"{FASTAPI_URL}/countries"
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def get_race():
+    response = requests.get(
+        f"{FASTAPI_URL}/racism"
+    )
+    response.raise_for_status()
+    return response.json()
+
+def jobs():
+    response = requests.get(
+        f"{FASTAPI_URL}/job_title"
+    )
+    response.raise_for_status()
+    return response.json()
