dab199b59d44f3aaa0ac99003d0f484f03155b21
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-16 13:28:19 +0100

    Refactor save_prediction function for consistency

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/database/crud.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/database/crud.py
@@ -3,15 +3,15 @@ from app.core.constants import Gend, Senio
 def save_prediction(db, data, Employee_Salary):
 
     db_obj = Prediction(
-        Age = data.Age,
-        Gender = Gend[data.Gender],
-        Education_Level = data.Education_Level,
-        Years_of_Experience = data.Years_of_Experience,
-        Country = data.Country,
-        Race = data.Race,
-        Senior = Senio[data.Senior],
-        Job_title = data.Job_title,
-        Employee_Salary = Employee_Salary)
+        Age=data.Age,
+        Gender=Gend[data.Gender],
+        Education_Level=data.Education_Level,
+        Years_of_Experience=data.Years_of_Experience,
+        Country=data.Country,
+        Race=data.Race,
+        Senior=Senio[data.Senior],
+        Job_title=data.Job_title,
+        Employee_Salary=Employee_Salary)
 
     db.add(db_obj)
     db.commit()
