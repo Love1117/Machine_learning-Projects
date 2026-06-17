9ccfe37873bb21575d778997e1e792e833afd56d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-17 20:22:22 +0100

    Update api_client.py

--- Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/frontend/api_client.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/frontend/api_client.py
@@ -3,9 +3,17 @@ import requests
 FASTAPI_URL = "http://fastapi:8000"
 
 def hand_digit(file):
+
+    files = {
+        "file": (file.name,
+                 file.getvalue(),
+                 file.type)
+    }
+
     response = requests.post(
         f"{FASTAPI_URL}/predict-image",
-        json=file
+        files=files
     )
+
     response.raise_for_status()
     return response.json()
