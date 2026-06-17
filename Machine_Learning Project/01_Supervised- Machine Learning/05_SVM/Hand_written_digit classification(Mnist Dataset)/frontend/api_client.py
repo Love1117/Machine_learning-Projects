432470134cbd379ce31056b2cc49d42eb7835435
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-17 18:31:08 +0100

    Add API client for hand-written digit classification

---
+++ Machine_Learning Project/01_Supervised- Machine Learning/05_SVM/Hand_written_digit classification(Mnist Dataset)/frontend/api_client.py
@@ -0,0 +1,11 @@
+import requests
+
+FASTAPI_URL = "http://fastapi:8000"
+
+def hand_digit(file):
+    response = requests.post(
+        f"{FASTAPI_URL}/predict",
+        json=file
+    )
+    response.raise_for_status()
+    return response.json()
