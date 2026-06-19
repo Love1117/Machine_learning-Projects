f2bfe9e66f3227d9c41111ac1ec6eb1b6b8c2e4e
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-19 02:50:22 +0100

    Refine prediction schema with literal types for status

--- Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit(segmentation)/app/schemas/prediction_schema.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit(segmentation)/app/schemas/prediction_schema.py
@@ -10,5 +10,5 @@ class PredictionRequest(BaseModel):
   Work_Experience: float= Field(..., example=8.0, description="work_experience")
   Spending_Score: int= Field(..., example=3, description="spending score")
   Family_Size: float= Field(..., example=3.0, description="work_experience")
-  Profession_status: str= Field(..., example="Doctor", description="profession")
-  Variable_status: str= Field(..., example="cat_2", description="input variable")
+  Profession_status: Literal["Artist", "Doctor", "Engineer", "Entertainment", "Executive", "Healthcare", "Homemaker", "Lawyer", "Marketing"]
+  Variable_status: Literal["Cat_1", "Cat_2", "Cat_3", "Cat_4", "Cat_5", "Cat_6", "Cat_7"]
