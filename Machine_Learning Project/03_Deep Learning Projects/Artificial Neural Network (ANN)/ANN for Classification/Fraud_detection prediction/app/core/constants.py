9e9cd8ccdb2f9f9efe0181aa35a107e07aab8201
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-20 14:49:54 +0100

    Update constants.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Fraud_detection prediction/app/core/constants.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network (ANN)/ANN for Classification/Fraud_detection prediction/app/core/constants.py
@@ -1,3 +1,4 @@
 VPN_USAGE = {"Yes":1, "No":0}
 PROXY_USAGE = {"Yes":1, "No":0}
 WEEKEND = {"Yes":1, "No":0}
+DAYS_OF_WEEK {"Monday":0, "Tuesday":1, "Wednesday":2, "Thursday":3, "Friday":4, "Saturday":5, "Sunday":6}
