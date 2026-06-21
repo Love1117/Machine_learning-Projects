fdcd98d3a9e142c1102463b92ccb6f24d87e05a6
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-22 00:36:01 +0100

    Update models.py

--- Machine_Learning Project/04_NLP Projects/Word2Vec Embeddings/Building Words with Similar meaning/app/database/models.py
+++ Machine_Learning Project/04_NLP Projects/Word2Vec Embeddings/Building Words with Similar meaning/app/database/models.py
@@ -1,21 +1,23 @@
 from sqlalchemy import Column, Integer, Float, String
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import JSON
+
 
 Base = declarative_base()
 
 
 class Prediction(Base):
-  __tablename__ = "predictions for similar_words"
+  __tablename__ = "similar_word_predictions"
 
   id = Column(Integer, primary_key=True, index=True)
   word = Column(String)
   topn = Column(Integer)
-  similar_words = Column(String)
+  similar_words = Column(JSON)
 
 
 
 class Prediction2(Base):
-  __tablename__ = "predictions for the similarity between two given words."
+  __tablename__ = "word_similarity_predictions"
 
   id = Column(Integer, primary_key=True, index=True)
   word1 = Column(String)
