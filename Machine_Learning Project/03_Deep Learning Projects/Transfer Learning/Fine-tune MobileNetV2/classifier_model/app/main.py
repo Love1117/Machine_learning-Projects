0c88d32da18ab62efaa19a3d469307c06c8ca033
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 02:45:43 +0100

    Update main.py

--- Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/classifier_model/app/main.py
+++ Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/classifier_model/app/main.py
@@ -9,13 +9,15 @@ app = FastAPI(
         title="Image classification API",
         version="1.0.0")
 
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
+
 @app.get("/")
 def read_root():
     return {"message": "Image classification FastAPI is Running!"}
 
 
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
-    
 app.include_router(router)
