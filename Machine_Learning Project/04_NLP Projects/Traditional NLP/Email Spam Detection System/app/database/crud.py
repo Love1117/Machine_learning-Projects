3090eebe00f797267c21189ae5905529b44b9d07
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 20:10:46 +0100

    Update crud.py

--- Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app/database/crud.py
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app/database/crud.py
@@ -3,7 +3,7 @@ from app.database.models import Prediction
 
 def save_prediction(db, text_input, prediction):
   db_obj = Prediction(
-        text=text_input.text,
+        text=text_input,
         prediction=prediction
     )
 
