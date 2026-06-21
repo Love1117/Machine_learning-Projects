c58a22f238e5161a7b72b153f8c0680a043d6888
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 20:15:33 +0100

    Update api_client.py

--- Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/frontend/api_client.py
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/frontend/api_client.py
@@ -2,7 +2,7 @@ import requests
 
 FASTAPI_URL = "http://fastapi:8000"
 
-def predict_next_word(text_input):
+def predict_mail(text_input):
     response = requests.post(
         f"{FASTAPI_URL}/predict",
         json=text_input
