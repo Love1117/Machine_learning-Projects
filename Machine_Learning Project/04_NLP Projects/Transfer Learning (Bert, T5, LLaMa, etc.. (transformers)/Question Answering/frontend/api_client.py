3696bc3b3e998a70998bbcde7fc989c3adfb67ea
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 21:56:19 +0100

    Create api_client.py

---
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning (Bert, T5, LLaMa, etc.. (transformers)/Question Answering/frontend/api_client.py
@@ -0,0 +1,11 @@
+import requests
+
+FASTAPI_URL = "http://fastapi:8000"
+
+def question(question_data):
+    response = requests.post(
+        f"{FASTAPI_URL}/predict_answer",
+        json=question_data
+    )
+    response.raise_for_status()
+    return response.json()
