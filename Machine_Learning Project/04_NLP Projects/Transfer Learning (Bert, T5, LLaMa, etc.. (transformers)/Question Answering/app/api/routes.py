edd84da49d76681429bacbd944da87e3d16e1b83
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 22:18:39 +0100

    Update routes.py

--- Machine_Learning Project/04_NLP Projects/Transfer Learning (Bert, T5, LLaMa, etc.. (transformers)/Question Answering/app/api/routes.py
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning (Bert, T5, LLaMa, etc.. (transformers)/Question Answering/app/api/routes.py
@@ -4,12 +4,19 @@ from sqlalchemy.orm import Session
 from app.schemas.prediction_schema import Question
 from app.services.predict_service import prediction
 from app.database.session import get_db
+from app.core.constants import qa_data
+
 
 router = APIRouter()
 
 @router.get("/model_check")
 def model_check():
     return {"status": "ok"}
+
+@router.get("/questions")
+def get_questions():
+    return list(qa_data.keys())
+
 
 
 @router.post("/predict_answer")
