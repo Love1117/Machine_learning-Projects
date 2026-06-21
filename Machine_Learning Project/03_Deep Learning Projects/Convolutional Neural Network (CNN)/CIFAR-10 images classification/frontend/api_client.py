bb4f907051f07ca2256ff693c83a63b79e0425d8
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 01:04:41 +0100

    Create api_client.py

---
+++ Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network (CNN)/CIFAR-10 images classification/frontend/api_client.py
@@ -0,0 +1,19 @@
+import requests
+
+FASTAPI_URL = "http://fastapi:8000"
+
+def image_class(file):
+
+    files = {
+        "file": (file.name,
+                 file.getvalue(),
+                 file.type)
+    }
+
+    response = requests.post(
+        f"{FASTAPI_URL}/predict",
+        files=files
+    )
+
+    response.raise_for_status()
+    return response.json()
