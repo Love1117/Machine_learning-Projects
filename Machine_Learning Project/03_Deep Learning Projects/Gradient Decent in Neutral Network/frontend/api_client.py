9d3e1ea037b394b697d666905ff49fef5a25a33c
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 01:57:18 +0100

    Create api_client.py

---
+++ Machine_Learning Project/03_Deep Learning Projects/Gradient Decent in Neutral Network/frontend/api_client.py
@@ -0,0 +1,11 @@
+import requests
+
+FASTAPI_URL = "http://fastapi:8000"
+
+def predict_grad(data):
+    response = requests.post(
+        f"{FASTAPI_URL}/predict",
+        json=data
+    )
+    response.raise_for_status()
+    return response.json()
