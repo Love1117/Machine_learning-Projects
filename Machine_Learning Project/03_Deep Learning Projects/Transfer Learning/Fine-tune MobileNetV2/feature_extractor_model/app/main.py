8447a7ececbb6ab6ddf10f3304833b3cf56dca8d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 03:12:28 +0100

    Update main.py

--- Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/feature_extractor_model/app/main.py
+++ Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/feature_extractor_model/app/main.py
@@ -9,13 +9,15 @@ app = FastAPI(
         title="Flower Photo classification API",
         version="1.0.0")
 
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
 @app.get("/")
 def read_root():
     return {"message": "Flower Photo FastAPI is Running!"}
 
 
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
-    
 app.include_router(router)
