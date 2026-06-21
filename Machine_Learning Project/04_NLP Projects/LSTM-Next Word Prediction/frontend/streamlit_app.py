c15472d11112e476048dbc962b9b4533d1460e47
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-21 20:22:45 +0100

    Update streamlit_app.py

--- Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/frontend/streamlit_app.py
+++ Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/frontend/streamlit_app.py
@@ -23,7 +23,7 @@ st.markdown("""
 <div class="main-header">
     <h1>Next Word Prediction</h1>
     <p>Generate the next words in a sentence using a LSTM deep learning model.</p>
-    <p>This model was trained on the novel <em>War and Peace</em> from Project Gutenberg and may perform best on text with a similar writing style.</p>
+    <p>This model was trained on the novel <em>crime and punishment</em> from Project Gutenberg and may perform best on text with a similar writing style.</p>
 </div>
 """, unsafe_allow_html=True)
 
