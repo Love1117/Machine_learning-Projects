2001abb60d950b9b82e5af8b17d53d2198effbd1
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-22 19:07:26 +0100

    Update main.py

--- Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/main.py
+++ Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/main.py
@@ -7,5 +7,11 @@ app = FastAPI(
     title="AI Assistant(chat bot) API",
     description="API for google/gemma-3-27b-it Chatbot/AI Assistant",
     version="1.0.0")
-    
+
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI is running"}
+
+
 app.include_router(router)
