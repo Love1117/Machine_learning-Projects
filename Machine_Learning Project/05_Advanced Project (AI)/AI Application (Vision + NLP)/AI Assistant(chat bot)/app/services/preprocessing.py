1121ff911627cabc79675ec9837a127556eee478
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-22 21:59:31 +0100

    Update preprocessing.py

--- Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/services/preprocessing.py
+++ Machine_Learning Project/05_Advanced Project (AI)/AI Application (Vision + NLP)/AI Assistant(chat bot)/app/services/preprocessing.py
@@ -1,6 +1,23 @@
 import base64
 from PIL import Image
+from faster_whisper import WhisperModel
+import tempfile
+import os
 import io
+
+
+model = WhisperModel("base", device="cpu", compute_type="int8")
+
+def transcribe_audio(audio_bytes):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+        temp_file.write(audio_bytes)
+        temp_path = temp_file.name
+    segments, info = model.transcribe(temp_path)
+    text = " ".join(
+        segment.text
+        for segment in segments)
+    os.remove(temp_path)
+    return text
 
 
 
