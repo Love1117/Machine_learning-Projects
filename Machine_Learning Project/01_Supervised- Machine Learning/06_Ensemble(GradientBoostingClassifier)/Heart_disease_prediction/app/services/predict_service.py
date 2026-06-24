2a709c5e3866123f9018825f746847a774a91892
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-24 13:59:04 +0100

    Update predict_service.py

--- Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble(GradientBoostingClassifier)/Heart_disease_prediction/app/services/predict_service.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble(GradientBoostingClassifier)/Heart_disease_prediction/app/services/predict_service.py
@@ -27,12 +27,12 @@ def prediction(data, db):
 
     scale_df = scale.transform(input_data)
 
-    prediction = model.predict(scale_df)[0]
+    Heart_Disease = model.predict(scale_df)[0]
 
     db_obj = save_prediction(db, data, Heart_Disease)
 
     return {
-        "Heart_Disease": "Yes" if prediction == 1 else "No",
+        "Heart_Disease": "Yes" if Heart_Disease == 1 else "No",
         "db_id": db_obj.id
     }
   except Exception as e:
