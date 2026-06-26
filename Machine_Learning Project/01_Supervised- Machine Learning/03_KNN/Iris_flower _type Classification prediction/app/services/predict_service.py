0441cc65696f2297ab1d12b287e199a5385c4013
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-26 09:23:16 +0100

    Update predict_service.py

--- Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/services/predict_service.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/services/predict_service.py
@@ -23,7 +23,7 @@ def prediction(data, db):
     else:
       iris_flower = "Verginca"
 
-    db_obj = save_prediction(db, data, prediction)
+    db_obj = save_prediction(db, data, iris_flower)
 
     return {
         "prediction": iris_flower,
