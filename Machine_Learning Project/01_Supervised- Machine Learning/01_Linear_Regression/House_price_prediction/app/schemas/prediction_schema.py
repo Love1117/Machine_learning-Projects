9ddcf288ec1daebc43013d4a2122fd86b6bc80a5
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-06-26 09:00:03 +0100

    Update prediction_schema.py

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/House_price_prediction/app/schemas/prediction_schema.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/House_price_prediction/app/schemas/prediction_schema.py
@@ -5,7 +5,7 @@ class PredictionRequest(BaseModel):
   Bedrooms: int= Field(..., example=2, description="fill in numbers of bedroom")
   Bathrooms: int= Field(..., example=3, description="fill in numbers of bathroom")
   Living_Space: int= Field(..., example=1538, description="fill in the size of Living Space")
-  Median_Household_Income: float= Field(..., example=370046, description="Median_Household_Income")
+  Median_Household_Income: float= Field(..., example=370046.00, description="Median_Household_Income")
   Zip_Code: int= Field(..., example=10017, description="Zip Code")
   Latitude: float= Field(..., example=40.72, description="Latitude")
   Longitude: float= Field(..., example=-74.00, description="Longitude")
