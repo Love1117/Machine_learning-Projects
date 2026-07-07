b73129ba5cdfd41b984ac12dd4c42ce84e89dca9
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-07 17:56:35 +0100

    Integrate SQLAdmin for Employee predictions management
    Added SQLAdmin integration for managing predictions.

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/main.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from app.api.routes import router
 from app.database.models import Base
 from app.database.session import engine
-
-
+from app.database.models import Prediction
+from sqladmin import Admin, ModelView
 
 app = FastAPI(title="Employee Salary Prediction",
               description="Production Style, ML Project for Employees Salary Prediction",
@@ -17,5 +17,17 @@ def on_startup():
 @app.get("/")
 def home():
     return{"message": "FastAPI is running successfully"}
+
+
+admin = Admin(app, engine)
+
+# 4. Tell SQLAdmin how to display your Product table
+class Employee_Admin(ModelView, model=Prediction):
+    column_list = [Product.id, Product.name, Product.price]
+    searchable_columns = [Product.name]
+    page_size = 50
+
+# Add the view to your admin dashboard
+admin.add_view(ProductAdmin)
 
 app.include_router(router)
