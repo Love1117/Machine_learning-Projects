4eb8b6b89c727aced840f59b5c7ae9b0147f361a
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-08 07:42:09 +0100

    Update FASTAPI_URL to new endpoint

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/frontend/api_client.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/frontend/api_client.py
@@ -1,6 +1,6 @@
 import requests
 
-FASTAPI_URL = "https://employee-salary-prediction-1mmm.onrender.com"
+FASTAPI_URL = "https://employee-salary-predict-ui.onrender.com"
 
 def predict_salary(data):
     response = requests.post(
