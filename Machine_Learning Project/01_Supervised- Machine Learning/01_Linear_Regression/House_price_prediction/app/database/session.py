563f3c7c5a80410cf70f4555925d3f9615ee851a
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-08 09:51:12 +0100

    Refactor database engine creation logic

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/House_price_prediction/app/database/session.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/House_price_prediction/app/database/session.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
