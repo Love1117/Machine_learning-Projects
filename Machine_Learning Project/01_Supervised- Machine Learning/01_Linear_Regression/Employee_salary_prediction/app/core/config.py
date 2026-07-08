41ff0976ccaeae3339866f1eb56e30045584d1bd
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-08 03:23:40 +0100

    Implement dotenv for environment variable management
    Added dotenv support to load environment variables and check for SECRET_KEY.

--- Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/core/config.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/01_Linear_Regression/Employee_salary_prediction/app/core/config.py
@@ -1,6 +1,18 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 MODEL_DIR = BASE_DIR / "models" / "1st_version"
 DATABASE_URL = "sqlite:///./employee_salary_prediction.db"
+
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("MY_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("MY_SECRET_KEY not found. Check your .env file.")
+  
