808391098bcfb74c9a188e4c086b1e13dbc4428e
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 20:19:51 +0100

    Update models.py

--- Machine_Learning Project/03_Deep Learning Projects/Gradient Decent in Neutral Network/app/database/models.py
+++ Machine_Learning Project/03_Deep Learning Projects/Gradient Decent in Neutral Network/app/database/models.py
@@ -5,7 +5,7 @@ Base = declarative_base()
 
 
 class Prediction(Base):
-  __tablename__ = "predictions"
+  __tablename__ = "gradient_churn_prediction_table"
 
   id = Column(Integer, primary_key=True, index=True)
   age = Column(Integer)
