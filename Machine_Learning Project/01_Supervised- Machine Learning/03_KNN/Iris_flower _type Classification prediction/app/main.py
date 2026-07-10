c897ce7e3080d687def75895ae74e091b2de1ab9
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 15:08:39 +0100

    Update main.py

--- Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/main.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/03_KNN/Iris_flower _type Classification prediction/app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.database.models import Base
+from app.database.models import Base, Prediction
 from app.database.session import engine
-
+from sqladmin import Admin, ModelView
+from app.core.config import SECRET_KEY, ADMIN_USERNAME, ADMIN_PASSWORD
+from sqladmin.authentication import AuthenticationBackend
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 
 app = FastAPI(title= "FastAPI for iris Flower type prediction",
               version="v1.0.0")
 
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 @app.on_event("startup")
 def on_startup():
@@ -16,5 +22,51 @@ def on_startup():
 @app.get("/")
 def home():
     return {"message": "Iris flower type is running"}
-    
+
+
+# 2. Create a Secure Admin Login System
+class AdminAuth(AuthenticationBackend):
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        username = form.get("username")
+        password = form.get("password")
+
+        # Set your secret admin credentials here.
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            request.session.update({"token": "authenticated_admin_user"})
+            return True
+        return False
+
+    async def logout(self, request: Request) -> bool:
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> bool:
+        token = request.session.get("token")
+        if token == "authenticated_admin_user":
+            return True
+        return False
+
+# Initialize the secure authentication backend
+authentication_backend = AdminAuth(secret_key=SECRET_KEY)
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+
+# 4. Tell SQLAdmin how to display your Product table
+class IrisAdmin(ModelView, model=Prediction):
+    column_list = [
+        Prediction.id,
+        Prediction.sepal_length,
+        Prediction.sepal_width,
+        Prediction.petal_length,
+        Prediction.petal_width,
+        Prediction.prediction,
+    ]
+
+    page_size = 50
+
+# Add the view to your admin dashboard
+admin.add_view(IrisAdmin)
+
 app.include_router(router)
