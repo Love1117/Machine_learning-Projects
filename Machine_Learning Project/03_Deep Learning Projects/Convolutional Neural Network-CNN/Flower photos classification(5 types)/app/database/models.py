1f7e829e759174f4824c66d2ed15c042c08da208
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 20:05:26 +0100

    Update models.py

--- Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network-CNN/Flower photos classification(5 types)/app/database/models.py
+++ Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network-CNN/Flower photos classification(5 types)/app/database/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 Base = declarative_base()
 
 class Prediction(Base):
-    __tablename__ = "predictions"
+    __tablename__ = "flower_photo_classification_table"
 
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String, nullable=False)
