124413d3528ca05b76093ef1eb45d95b75047f6a
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 20:04:31 +0100

    Update config.py

--- Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network-CNN/Flower photos classification(5 types)/app/core/config.py
+++ Machine_Learning Project/03_Deep Learning Projects/Convolutional Neural Network-CNN/Flower photos classification(5 types)/app/core/config.py
@@ -1,6 +1,30 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 MODEL_DIR = BASE_DIR / "models" / "1st_version"
-DATABASE_URL = "sqlite:///./Flowers_photos_classification.db"
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not found. Check your .env file.")
+        
+
+SECRET_KEY = os.getenv("MY_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("MY_SECRET_KEY not found. Check your .env file.")
+
+
+ADMIN_USERNAME = os.getenv("USERNAME")
+if not ADMIN_USERNAME:
+    raise ValueError("USERNAME not found. Check your .env file.")
+
+
+ADMIN_PASSWORD = os.getenv("PASSWORD")
+if not ADMIN_PASSWORD:
+    raise ValueError("PASSWORD not found. Check your .env file.")
