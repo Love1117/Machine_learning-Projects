9170207393aaf03f7dd86f5ba5f2d50d5e7e499f
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 15:55:14 +0100

    Update main.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit-segmentation/app/main.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit-segmentation/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.database.models import Base
+from app.database.models import Base, Prediction
 from app.database.session import engine
+from sqladmin import Admin, ModelView
+from app.core.config import SECRET_KEY, ADMIN_USERNAME, ADMIN_PASSWORD
+from sqladmin.authentication import AuthenticationBackend
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 
 
@@ -9,6 +15,8 @@ app = FastAPI(
     title="Customer Spending Habit(segmentation) API",
     description="Production-ready ML API"
 )
+
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 
 @app.on_event("startup")
@@ -19,6 +27,66 @@ def on_startup():
 @app.get("/")
 def home():
     return {"message": "Fast API is running"}
+
+
+# 2. Create a Secure Admin Login System
+class AdminAuth(AuthenticationBackend):
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        username = form.get("username")
+        password = form.get("password")
+
+        # Set your secret admin credentials here.
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            request.session.update({"token": "authenticated_admin_user"})
+            return True
+        return False
+
+    async def logout(self, request: Request) -> bool:
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> bool:
+        token = request.session.get("token")
+        if token == "authenticated_admin_user":
+            return True
+        return False
+
+# Initialize the secure authentication backend
+authentication_backend = AdminAuth(secret_key=SECRET_KEY)
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+
+# 4. Tell SQLAdmin how to display your Product table
+class SpendingAdmin(ModelView, model=Prediction):
+    column_list = [
+        Prediction.id,
+        Prediction.Gender,
+        Prediction.Ever_Married,
+        Prediction.Age,
+        Prediction.Graduated,
+        Prediction.Work_Experience,
+        Prediction.Spending_Score,
+        Prediction.Family_Size,
+        Prediction.Profession_status,
+        Prediction.Variable_status,
+        Prediction.prediction,
+    ]
+
+    searchable_columns = [
+        Prediction.Graduated,
+        Prediction.Ever_Married,
+        Prediction.Gender,
+        Prediction.Profession_status,
+        Prediction.Variable_status,
+        Prediction.prediction,
+    ]
+
+    page_size = 50
+
+# Add the view to your admin dashboard
+admin.add_view(SpendingAdmin)
 
 
 app.include_router(router)
