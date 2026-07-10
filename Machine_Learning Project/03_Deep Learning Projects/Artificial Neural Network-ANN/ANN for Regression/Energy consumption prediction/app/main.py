e83d638843891b95a8bd501e9af15bd2c0777fa2
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 19:55:03 +0100

    Update main.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network-ANN/ANN for Regression/Energy consumption prediction/app/main.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network-ANN/ANN for Regression/Energy consumption prediction/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.database.models import Base
+from app.database.models import Base, Prediction
 from app.database.session import engine
+from sqladmin import Admin, ModelView
+from app.core.config import SECRET_KEY, ADMIN_USERNAME, ADMIN_PASSWORD
+from sqladmin.authentication import AuthenticationBackend
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 
 
@@ -9,6 +15,9 @@ app = FastAPI(
     title="Energy Consumption Prediction API",
     description="Production-ready ML API"
 )
+
+
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 
 @app.on_event("startup")
@@ -19,5 +28,63 @@ def on_startup():
 @app.get("/")
 def home():
     return {"message": "FastAPI is running"}
-    
+
+
+# 2. Create a Secure Admin Login System
+class AdminAuth(AuthenticationBackend):
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        username = form.get("username")
+        password = form.get("password")
+
+        # Set your secret admin credentials here.
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            request.session.update({"token": "authenticated_admin_user"})
+            return True
+        return False
+
+    async def logout(self, request: Request) -> bool:
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> bool:
+        token = request.session.get("token")
+        if token == "authenticated_admin_user":
+            return True
+        return False
+
+# Initialize the secure authentication backend
+authentication_backend = AdminAuth(secret_key=SECRET_KEY)
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+
+# 4. Tell SQLAdmin how to display your Product table
+class EnergyAdmin(ModelView, model=Prediction):
+    column_list = [
+        Prediction.id,
+        Prediction.Home_ID,
+        Prediction.Outdoor_Temperature_C,
+        Prediction.Household_Size,
+        Prediction.Year,
+        Prediction.Month,
+        Prediction.Day,
+        Prediction.Days_Of_The_Week,
+        Prediction.Hour,
+        Prediction.Weekend,
+        Prediction.Appliance_Type_status,
+        Prediction.Season_status,
+        Prediction.Energy_Consumption_kWh,
+    ]
+
+    searchable_columns = [
+        Prediction.Appliance_Type_status,
+        Prediction.Season_status,
+    ]
+
+    page_size = 50
+
+# Add the view to your admin dashboard
+admin.add_view(EnergyAdmin)
+
 app.include_router(router)
