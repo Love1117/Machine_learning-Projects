47e3b3f6e9d5e52811a4c799f44a92987e982d33
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 20:56:18 +0100

    Update main.py

--- Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/feature_extractor_model/app/main.py
+++ Machine_Learning Project/03_Deep Learning Projects/Transfer Learning/Fine-tune MobileNetV2/feature_extractor_model/app/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.database.models import Base
+from app.database.models import Base, Prediction
 from app.database.session import engine
-
+from sqladmin import Admin, ModelView
+from app.core.config import SECRET_KEY, ADMIN_USERNAME, ADMIN_PASSWORD
+from sqladmin.authentication import AuthenticationBackend
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 
 app = FastAPI(
         title="Flower Photo classification API",
         version="1.0.0")
 
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 @app.on_event("startup")
 def on_startup():
@@ -18,6 +24,61 @@ def on_startup():
 @app.get("/")
 def read_root():
     return {"message": "Flower Photo FastAPI is Running!"}
+
+
+# 2. Create a Secure Admin Login System
+class AdminAuth(AuthenticationBackend):
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        username = form.get("username")
+        password = form.get("password")
+
+        # Set your secret admin credentials here.
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            request.session.update({"token": "authenticated_admin_user"})
+            return True
+        return False
+
+    async def logout(self, request: Request) -> bool:
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> bool:
+        token = request.session.get("token")
+        if token == "authenticated_admin_user":
+            return True
+        return False
+
+# Initialize the secure authentication backend
+authentication_backend = AdminAuth(secret_key=SECRET_KEY)
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+ id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, nullable=False)
+    predicted_class_name = Column(Integer, nullable=False)
+    confidence = Column(Float)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+# 4. Tell SQLAdmin how to display your Product table
+class VectorAdmin(ModelView, model=Prediction):
+    column_list = [
+        Prediction.id,
+        Prediction.filename,
+        Prediction.predicted_class_name,
+        Prediction.confidence,
+        Prediction.created_at,
+    ]
+
+    searchable_columns = [
+        Prediction.predicted_class_name,
+    ]
+
+    page_size = 50
+
+# Add the view to your admin dashboard
+admin.add_view(VectorAdmin)
 
 
 app.include_router(router)
