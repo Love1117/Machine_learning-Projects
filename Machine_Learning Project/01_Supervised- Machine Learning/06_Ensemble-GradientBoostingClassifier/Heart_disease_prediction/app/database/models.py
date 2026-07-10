b4f30c316d178b5862a0883f811ab0aedff7f734
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-10 15:20:23 +0100

    Update models.py

--- Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble-GradientBoostingClassifier/Heart_disease_prediction/app/database/models.py
+++ Machine_Learning Project/01_Supervised- Machine Learning/06_Ensemble-GradientBoostingClassifier/Heart_disease_prediction/app/database/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 class Prediction(Base):
-    __tablename__ = "predictions"
+    __tablename__ = "heart_disease_prediction_table"
 
     id = Column(Integer, primary_key=True, index=True)
     gender = Column(String)
