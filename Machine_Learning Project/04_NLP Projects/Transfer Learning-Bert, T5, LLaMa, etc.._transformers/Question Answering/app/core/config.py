463c25985acaa844d4a2261d6d5d3c2d9fc60077
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-11 13:54:25 +0100

    Update config.py

--- Machine_Learning Project/04_NLP Projects/Transfer Learning-Bert, T5, LLaMa, etc.._transformers/Question Answering/app/core/config.py
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning-Bert, T5, LLaMa, etc.._transformers/Question Answering/app/core/config.py
@@ -1,1 +1,24 @@
-DATABASE_URL = "sqlite:///./Question_answering.db"
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not found. Check your .env file.")
+        
+
+SECRET_KEY = os.getenv("MY_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("MY_SECRET_KEY not found. Check your .env file.")
+
+
+ADMIN_USERNAME = os.getenv("USERNAME")
+if not ADMIN_USERNAME:
+    raise ValueError("USERNAME not found. Check your .env file.")
+
+
+ADMIN_PASSWORD = os.getenv("PASSWORD")
+if not ADMIN_PASSWORD:
+    raise ValueError("PASSWORD not found. Check your .env file.")
