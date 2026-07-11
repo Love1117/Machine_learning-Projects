248428511b9bd36daee5350bd5fd7380060abaee
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-11 13:36:40 +0100

    Update models.py

--- Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/database/models.py
+++ Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/database/models.py
@@ -5,7 +5,7 @@ Base = declarative_base()
 
 
 class Prediction(Base):
-  __tablename__ = "predictions"
+  __tablename__ = "next_word_prediction_table"
 
   id = Column(Integer, primary_key=True, index=True)
   input_word = Column(String)
