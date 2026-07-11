ac6920601c9265cc17f3a7a4bae4d7be9437eee2
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-11 13:17:28 +0100

    Update session.py

--- Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app/database/session.py
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app/database/session.py
@@ -2,14 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(bind=engine)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
