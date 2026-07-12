0ab1c3841fcc93d8f65cd71f7e36530e6706dd5c
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-12 18:56:56 +0100

    Update model_loader.py

--- Machine_Learning Project/05_Advanced Project_AI/Recommendation System/Music_Recommendation_System/app/services/model_loader.py
+++ Machine_Learning Project/05_Advanced Project_AI/Recommendation System/Music_Recommendation_System/app/services/model_loader.py
@@ -2,5 +2,5 @@ import joblib
 from app.core.config import MODEL_DIR
 
 
-matrix = joblib.load(MODEL_DIR, "matrix.joblib")
-songs = joblib.load(MODEL_DIR, "songs.joblib")
+matrix = joblib.load(MODEL_DIR / "matrix.joblib")
+songs = joblib.load(MODEL_DIR / "songs.joblib")
