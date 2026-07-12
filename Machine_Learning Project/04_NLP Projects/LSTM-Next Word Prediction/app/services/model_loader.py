6a81024eef68608ad9bcd3b0b508cd5b1b362cd3
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-12 04:06:18 +0100

    Update model_loader.py

--- Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/app/services/model_loader.py
+++ Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/app/services/model_loader.py
@@ -1,46 +1,28 @@
 import joblib
 import tensorflow as tf
 from tensorflow import keras
-import gdown
-from app.core.config import MODEL_DIR
+from huggingface_hub import hf_hub_download
 import os
+from dotenv import load_dotenv
 
-BASE_DIR = Path.cwd()
 
-MODEL_PATH = MODEL_DIR / "my_model_2.keras"
-TOKENIZER_PATH = MODEL_DIR / "tokenizer.joblib"
+load_dotenv()
 
-# 🔹 Google Drive File IDs
-MODEL_FILE_ID = "1fJpCz2pY_02P9IOdxe5pNu9-GOCx-0Hi"
-TOKENIZER_FILE_ID = "1ZbZ6Hlesmi4MzgmHZIe216-o4LrDMCOi"
+HF_GRU_TOKEN = os.getenv("HF_GRU_TOKEN")
 
-def download_from_drive(file_id, output_path):
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, str(output_path), quiet=False)
+REPO_ID = "Love117/lstm-prediction"
 
-def ensure_files_exist():
-    MODEL_DIR.mkdir(parents=True, exist_ok=True)
+MODEL_PATH = hf_hub_download(
+    repo_id=REPO_ID,
+    filename="my_model_2.keras",
+    token=HF_GRU_TOKEN,
+)
 
-    # Download model if not exists
-    if not MODEL_PATH.exists():
-        print("Downloading model from Google Drive...")
-        download_from_drive(MODEL_FILE_ID, MODEL_PATH)
+TOKENIZER_PATH = hf_hub_download(
+    repo_id=REPO_ID,
+    filename="tokenizer.joblib",
+    token=HF_GRU_TOKEN,
+)
 
-    # Download tokenizer if not exists
-    if not TOKENIZER_PATH.exists():
-        print("Downloading tokenizer from Google Drive...")
-        download_from_drive(TOKENIZER_FILE_ID, TOKENIZER_PATH)
-
-@app.on_event("startup")
-def load_resources():
-    global model, tokenizer
-
-    ensure_files_exist()
-
-    print("Loading model...")
-    model = load_model(MODEL_PATH)
-
-    print("Loading tokenizer...")
-    tokenizer = joblib.load(TOKENIZER_PATH)
-
-    print("✅ Model and Tokenizer loaded successfully!")
+model = keras.models.load_model(MODEL_PATH)
+tokenizer = joblib.load(TOKENIZER_PATH)
