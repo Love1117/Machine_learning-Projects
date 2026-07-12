6f2737effec12e4472586dd161e4b97a37a2ba3d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-12 04:15:37 +0100

    Update model_loader.py

--- Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/app/services/model_loader.py
+++ Machine_Learning Project/04_NLP Projects/LSTM-Next Word Prediction/app/services/model_loader.py
@@ -1,28 +1,38 @@
+import os
 import joblib
-import tensorflow as tf
 from tensorflow import keras
 from huggingface_hub import hf_hub_download
-import os
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 HF_GRU_TOKEN = os.getenv("HF_GRU_TOKEN")
 
+if not HF_GRU_TOKEN:
+    raise ValueError("HF_GRU_TOKEN not found.")
+
 REPO_ID = "Love117/lstm-prediction"
 
-MODEL_PATH = hf_hub_download(
-    repo_id=REPO_ID,
-    filename="my_model_2.keras",
-    token=HF_GRU_TOKEN,
-)
+model = None
+tokenizer = None
 
-TOKENIZER_PATH = hf_hub_download(
-    repo_id=REPO_ID,
-    filename="tokenizer.joblib",
-    token=HF_GRU_TOKEN,
-)
 
-model = keras.models.load_model(MODEL_PATH)
-tokenizer = joblib.load(TOKENIZER_PATH)
+def load_resources():
+    global model, tokenizer
+
+    model_path = hf_hub_download(
+        repo_id=REPO_ID,
+        filename="my_model_2.keras",
+        token=HF_GRU_TOKEN,
+    )
+
+    tokenizer_path = hf_hub_download(
+        repo_id=REPO_ID,
+        filename="tokenizer.joblib",
+        token=HF_GRU_TOKEN,
+    )
+
+    model = keras.models.load_model(model_path)
+    tokenizer = joblib.load(tokenizer_path)
+
+    print("✅ GRU model loaded successfully.")
