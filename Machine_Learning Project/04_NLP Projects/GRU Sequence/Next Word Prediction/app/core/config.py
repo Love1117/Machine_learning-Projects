2aa2e0b60215e6962516e58c261cc6f67dfa4daf
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-12 02:15:17 +0100

    Update config.py

--- Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/core/config.py
+++ Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/core/config.py
@@ -1,11 +1,6 @@
-from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-MODEL_DIR = BASE_DIR / "models" / "1st_version"
 
 
 load_dotenv()
