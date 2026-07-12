68899c492928727ba1740a146011bc015baa8f0d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-12 04:12:53 +0100

    Load ML model and tokenizer on startup
    Added resource loading during application startup.

--- Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/main.py
+++ Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/main.py
@@ -21,6 +21,8 @@ app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
+    # Load ML model and tokenizer
+    load_resources()
 
 
 @app.get("/")
