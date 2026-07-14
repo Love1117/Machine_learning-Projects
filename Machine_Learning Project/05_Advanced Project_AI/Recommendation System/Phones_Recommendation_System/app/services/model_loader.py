403e0e9f55290f27cbd8f5cab2d1640b20ae3ae2
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-14 01:57:42 +0100

    Update model_loader.py

--- Machine_Learning Project/05_Advanced Project_AI/Recommendation System/Phones_Recommendation_System/app/services/model_loader.py
+++ Machine_Learning Project/05_Advanced Project_AI/Recommendation System/Phones_Recommendation_System/app/services/model_loader.py
@@ -2,5 +2,5 @@ import joblib
 from app.core.config import MODEL_DIR
 
 
-phone_data = joblib.load(MODEL_DIR, "phone_data.joblib")
-vector = joblib.load(MODEL_DIR, "vector.joblib")
+phone_data = joblib.load(MODEL_DIR / "phone_data.joblib")
+vector = joblib.load(MODEL_DIR / "vector.joblib")
