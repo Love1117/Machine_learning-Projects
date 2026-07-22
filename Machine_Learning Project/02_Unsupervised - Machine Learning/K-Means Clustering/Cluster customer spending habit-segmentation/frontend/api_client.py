5ae7cf264c1c085364e467e0aeceb451be62a5b2
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-22 02:38:30 +0100

    Update api_client.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit-segmentation/frontend/api_client.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit-segmentation/frontend/api_client.py
@@ -1,6 +1,6 @@
 import requests
 
-FASTAPI_URL = "http://fastapi:8000"
+FASTAPI_URL = "https://customer-spending-habit-ui.onrender.com"
 
 def predict_spend_habit(data):
     response = requests.post(
