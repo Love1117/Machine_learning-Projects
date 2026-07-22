6256b5170aa639d36c9f4faa01dd454b237bfca9
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-22 02:58:44 +0100

    Update prediction_schema.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit-segmentation/app/schemas/prediction_schema.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/K-Means Clustering/Cluster customer spending habit-segmentation/app/schemas/prediction_schema.py
@@ -7,8 +7,8 @@ class PredictionRequest(BaseModel):
   Ever_Married: Literal["Yes", "No"]
   Age: int= Field(..., example=36, description="input your age")
   Graduated: Literal["Yes", "No"]
-  Work_Experience: int= Field(..., example=8, description="work_experience")
+  Work_Experience: float= Field(..., example=8.0, description="work_experience")
   Spending_Score: int= Field(..., example=3, description="spending score")
-  Family_Size: int= Field(..., example=3, description="Family size")
+  Family_Size: float= Field(..., example=3.0, description="Family size")
   Profession_status: Literal["Artist", "Doctor", "Engineer", "Entertainment", "Executive", "Healthcare", "Homemaker", "Lawyer", "Marketing"]
   Variable_status: Literal["Cat_1", "Cat_2", "Cat_3", "Cat_4", "Cat_5", "Cat_6", "Cat_7"]
