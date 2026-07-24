b6244dc340077511bbe6d55948747c8601e993a7
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-24 17:49:52 +0100

    Update model_loader.py

--- Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network-ANN/ANN for Classification/Customer_churn_prediction/app/services/model_loader.py
+++ Machine_Learning Project/03_Deep Learning Projects/Artificial Neural Network-ANN/ANN for Classification/Customer_churn_prediction/app/services/model_loader.py
@@ -1,5 +1,7 @@
 import joblib
+import tensorflow as tf
+from tensorflow import keras
 from app.core.config import MODEL_DIR
 
-model = None
+model = keras.models.load_model(MODEL_DIR / "Customer_churn_prediction_V1.0.0.keras")
 scale = joblib.load(MODEL_DIR / "scale.joblib")
