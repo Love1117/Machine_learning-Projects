654f6ad46157b28b1f0a72e92615dde668f47338
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 14:40:09 +0100

    Update preprocessing.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/app/services/preprocessing.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/app/services/preprocessing.py
@@ -1,5 +1,5 @@
-def encode_type(type_status):
-  return {"type_CASH_OUT": 1 if type_status == "CASH_OUT" else 0,
-           "type_DEBIT": 1 if type_status == "DEBIT" else 0,
-           "type_PAYMENT": 1 if type_status == "PAYMENT" else 0,
-           "type_TRANSFER": 1 if type_status == "TRANSFER" else 0}
+def encode_type(status_type):
+  return {"type_CASH_OUT": 1 if status_type == "CASH_OUT" else 0,
+           "type_DEBIT": 1 if status_type == "DEBIT" else 0,
+           "type_PAYMENT": 1 if status_type == "PAYMENT" else 0,
+           "type_TRANSFER": 1 if status_type == "TRANSFER" else 0}
