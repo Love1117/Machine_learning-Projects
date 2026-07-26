0adbdffa25a1b324a18f0e7b443e38a33364339b
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 06:32:06 +0100

    Fix tokenizer variable name and loading message

--- Machine_Learning Project/04_NLP Projects/Transfer Learning_Bert-T5-LLaMa-transformers/Sentiment Analysis/app/services/model_loader.py
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning_Bert-T5-LLaMa-transformers/Sentiment Analysis/app/services/model_loader.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
-model_name = "distilbert-base-uncased-finetuned-sst-2-english"roberta_tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+Distilbert_tokenizer = AutoTokenizer.from_pretrained(model_name)
 Distilbert_model = AutoModelForSequenceClassification.from_pretrained(model_name)
 print("Roberta model loaded successfully.")
