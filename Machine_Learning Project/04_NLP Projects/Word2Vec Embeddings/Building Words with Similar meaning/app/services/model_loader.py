52d7e3b9aee5ea553b9138535f4c2dd76e6ae060
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 04:44:59 +0100

    Update model_loader.py

--- Machine_Learning Project/04_NLP Projects/Word2Vec Embeddings/Building Words with Similar meaning/app/services/model_loader.py
+++ Machine_Learning Project/04_NLP Projects/Word2Vec Embeddings/Building Words with Similar meaning/app/services/model_loader.py
@@ -3,4 +3,4 @@ import os
 from pathlib import Path
 from app.core.config import MODEL_DIR
 
-word2vec_model = gensim.models.Word2Vec.load(MODEL_DIR / "netflix_text_reviews.model")
+word2vec_model = gensim.models.Word2Vec.load(str(MODEL_DIR / "netflix_text_reviews.model"))
