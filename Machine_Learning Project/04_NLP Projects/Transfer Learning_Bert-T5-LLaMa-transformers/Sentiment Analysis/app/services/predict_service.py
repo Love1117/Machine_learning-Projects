d6580230edc96ceb827c3c81acde829ef3dc432d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 06:34:54 +0100

    Switch model from Roberta to DistilBERT for predictions

--- Machine_Learning Project/04_NLP Projects/Transfer Learning_Bert-T5-LLaMa-transformers/Sentiment Analysis/app/services/predict_service.py
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning_Bert-T5-LLaMa-transformers/Sentiment Analysis/app/services/predict_service.py
@@ -1,27 +1,27 @@
 from fastapi import HTTPException
 import torch
 from scipy.special import softmax
-from app.services.model_loader import roberta_tokenizer, roberta_model
+from app.services.model_loader import Distilbert_tokenizer, Distilbert_model
 from app.database.crud import save_prediction
 
 def prediction(request, db):
   try:
-    inputs = roberta_tokenizer(request.text, truncation=True, padding=True, max_length=512, return_tensors="pt")
+    inputs = Distilbert_tokenizer(request.text, truncation=True, padding=True, max_length=512, return_tensors="pt")
     with torch.no_grad():
-        outputs = roberta_model(**inputs)
+        outputs = DistilBERT_model(**inputs)
 
     logits = outputs.logits[0].detach().numpy()
     scores = softmax(logits)
 
-    rob_dict = {"Roberta_neg": float(scores[0]),
-                "Roberta_neu": float(scores[1]),
-                "Roberta_pos": float(scores[2]),
+    rob_dict = {"Distilbert_neg": float(scores[0]),
+                "Distilbert_neu": float(scores[1]),
+                "Distilbert_pos": float(scores[2]),
                }
 
-    db_obj = save_prediction(db=db, model="Roberta", request=request, sentiment_scores=rob_dict)
+    db_obj = save_prediction(db=db, model="Distilbert", request=request, sentiment_scores=rob_dict)
 
     return {
-        "model": "Roberta", 
+        "model": "Distilbert", 
         "text": request.text, 
         "sentiment_scores": rob_dict,
         "db_id": db_obj.id}
