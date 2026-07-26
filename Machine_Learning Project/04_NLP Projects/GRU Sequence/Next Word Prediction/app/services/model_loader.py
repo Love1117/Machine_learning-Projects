d17f22c7946aa5757053f38a7d2909ce6ebe24b3
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 13:36:00 +0100

    Update model_loader.py

--- Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/services/model_loader.py
+++ Machine_Learning Project/04_NLP Projects/GRU Sequence/Next Word Prediction/app/services/model_loader.py
@@ -19,27 +19,21 @@ tokenizer = None
 
 def load_resources():
     global model, tokenizer
-    print("Step 1")
 
     model_path = hf_hub_download(
         repo_id=REPO_ID,
         filename="GRU_model.keras",
         token=HF_GRU_TOKEN,
     )
-    print("Step 2")
 
     tokenizer_path = hf_hub_download(
-        repo_id=REPO_ID,
+        repo_id=REPO_ID,-
         filename="GRU_tokenizer.pkl",
         token=HF_GRU_TOKEN,
     )
-    print("Step 3")
 
-    # model = keras.models.load_model(model_path)
-    model = None
+    model = keras.models.load_model(model_path)
     
-    # tokenizer = joblib.load(tokenizer_path)
-    tokenizer = None
-    print("Step 4")
+    tokenizer = joblib.load(tokenizer_path)
 
     print("✅ GRU model loaded successfully.")
