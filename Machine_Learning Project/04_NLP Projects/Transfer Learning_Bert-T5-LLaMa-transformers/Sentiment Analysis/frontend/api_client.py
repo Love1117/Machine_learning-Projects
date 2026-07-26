34cd9ef15c955680f7c8256ecb1c042969a38f50
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 06:48:50 +0100

    Change API endpoint from predict-roberta to predict-distilbert

--- Machine_Learning Project/04_NLP Projects/Transfer Learning_Bert-T5-LLaMa-transformers/Sentiment Analysis/frontend/api_client.py
+++ Machine_Learning Project/04_NLP Projects/Transfer Learning_Bert-T5-LLaMa-transformers/Sentiment Analysis/frontend/api_client.py
@@ -4,7 +4,7 @@ FASTAPI_URL = "http://fastapi:8000"
 
 def sentiment(request):
     response = requests.post(
-        f"{FASTAPI_URL}/predict-roberta",
+        f"{FASTAPI_URL}/predict-distilbert",
         json=request
     )
     response.raise_for_status()
