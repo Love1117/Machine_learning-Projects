40179825ed60ad159e8e7c8fca8775deead7ccf0
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-07-26 05:15:16 +0100

    Update crud.py

--- Machine_Learning Project/04_NLP Projects/Word2Vec Embeddings/Building Words with Similar meaning/app/database/crud.py
+++ Machine_Learning Project/04_NLP Projects/Word2Vec Embeddings/Building Words with Similar meaning/app/database/crud.py
@@ -14,11 +14,11 @@ def save_prediction(db, request, result):
 
 
 
-def save_prediction2(db, request, float(similarity)):
+def save_prediction2(db, request, similarity):
   db_obj_one = Prediction2(
         word1=request.word1,
         word2=request.word2,
-        similarity_score=similarity)
+        similarity_score=float(similarity))
   db.add(db_obj_one)
   db.commit()
   db.refresh(db_obj_one)
