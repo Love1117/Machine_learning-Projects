ed20d1503a3df35b30f7639130bd6f5e33d2539d
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-08-12 20:11:23 +0100

    Update predict_service.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/app/services/predict_service.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/app/services/predict_service.py
@@ -30,11 +30,25 @@ def prediction(data, db):
 
     prediction = is_fraud
 
-    db_obj = save_prediction(db, data, prediction)
+    db_id = None
+    database_saved = False
+    
+    try:
+        db_obj = save_prediction(db, data,  prediction)
+        db_id = db_obj.id
+        database_saved = True
+     
+    except Exception as db_error:
+        traceback.print_exc()
+        try:
+            db.rollback()
+        except Exception:
+            pass
 
     return {
         "prediction": is_fraud,
-        "db_id": db_obj.id
+        "db_id": db_id,
+        "database_saved": database_saved
     }
   except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
+    raise HTTPException(status_code=500, detail="An error occurred while processing the prediction.")
