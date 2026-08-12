b5855d14141532ca4df50f8976c665e0cafcccef
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-08-12 21:11:03 +0100

    Update predict_service.py

--- Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app/services/predict_service.py
+++ Machine_Learning Project/04_NLP Projects/Traditional NLP/Email Spam Detection System/app/services/predict_service.py
@@ -12,13 +12,29 @@ def prediction(text_input, db):
     prediction = model.predict(new_vec)
 
     result = "Spam" if prediction[0] == 1 else "Not-Spam"
+
+
+    db_id = None
+    database_saved = False
     
-    db_obj = save_prediction(db, text_input, prediction)
+    try:
+        db_obj = save_prediction(db, text_input,  prediction)
+        db_id = db_obj.id
+        database_saved = True
+     
+    except Exception as db_error:
+        traceback.print_exc()
+        try:
+            db.rollback()
+        except Exception:
+            pass
+          
 
     return {
         "input_text": text_input.text, 
         "prediction": result,
-        "db_id": db_obj.id
+        "db_id": db_id,
+        "database_saved": database_saved
     }
   except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
+    raise HTTPException(status_code=500, detail="An error occurred while processing the prediction.")
