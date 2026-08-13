535e605548fe0ca7e96a730590a3ae9accaf3444
Loveday Shadrack <lovedayshadrack3@gmail.com>
2026-08-13 20:12:26 +0100

    Update api_client.py

--- Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/frontend/api_client.py
+++ Machine_Learning Project/02_Unsupervised - Machine Learning/DBSCAN/CreditCard_Fraud_Detection/frontend/api_client.py
@@ -2,17 +2,10 @@ import requests
 
 FASTAPI_URL = "https://credit-card-fraud-detection-api-hnt3.onrender.com"
 
-
 def predict_fraud(data):
     response = requests.post(
         f"{FASTAPI_URL}/predict",
-        json=data,
-        timeout=60
+        json=data
     )
-
-if response.status_code != 200:
-    print("STATUS:", response.status_code)
-    print("RESPONSE:", response.text)
-
-    raise Exception(
-        f"FastAPI Error {response.status_code}: {response.text}")
+    response.raise_for_status()
+    return response.json()
